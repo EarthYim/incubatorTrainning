aa7e3458b2ad76f3056adbf1773a250679893f58
Kasipat Wongsamran <kasipat.w@arise.tech>
2023-08-16 10:08:02 +0700

    add refactor default

--- dbgo/wallet/db_test.go
+++ dbgo/wallet/db_test.go
@@ -1,6 +1,7 @@
 package wallet
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"testing"
 
@@ -16,11 +17,20 @@ import (
 // }
 
 func TestInserWalletDummy(t *testing.T) {
-	db, _, err := sqlmock.New()
+	db, mock, err := sqlmock.New()
 	if err != nil {
 		fmt.Println("error creating mock database")
 		return
 	}
+	defer db.Close()
+
+	query := "INSERT INTO wallet"
+	expectedArgs := []driver.Value{"", 0.0}
+
+	// Expect QueryRow to be called with the provided query and args
+	mock.ExpectQuery(query).WithArgs(expectedArgs...).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(-1)) // Mock returned row
+
 	var id int
 	err, id = InsertWallet(db, "", 0)
 	if err != nil {
@@ -38,15 +48,23 @@ type StubOwner struct {
 }
 
 func TestInsertWalletStub(t *testing.T) {
-	db, _, err := sqlmock.New()
+	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Errorf("error creating mock databse")
 	}
 
+	defer db.Close()
+
+	query := "INSERT INTO wallet"
 	var Owner = StubOwner{
 		owner:   "Earth",
 		balance: 100.0,
 	}
+	expectedArgs := []driver.Value{Owner.owner, Owner.balance}
+
+	// Expect QueryRow to be called with the provided query and args
+	mock.ExpectQuery(query).WithArgs(expectedArgs...).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1)) // Mock returned row
 
 	var id int
 	err, id = InsertWallet(db, Owner.owner, Owner.balance)
@@ -58,4 +76,14 @@ func TestInsertWalletStub(t *testing.T) {
 	if id != want {
 		t.Errorf("Want '%d', got '%d'", want, id)
 	}
+}
+
+func TestInsertWalletFake(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error("error creating mock database")
+	}
+	defer db.close()
+
+	query := "INSERT INTO wallet"
 }
